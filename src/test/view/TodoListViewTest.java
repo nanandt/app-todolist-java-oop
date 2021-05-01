b8913fc134372cddbf0c9517e06adfaae8694aa9
nanandt <rizkyfatih007@gmail.com>
2021-05-01 10:56:44 +0700

    test view add todolist

---
+++ src/test/view/TodoListViewTest.java
@@ -0,0 +1,36 @@
+package test.view;
+
+import repository.TodoListRepository;
+import repository.TodoListRepositoryImpl;
+import service.TodoListService;
+import service.TodoListServiceImpl;
+import view.TodoListView;
+
+public class TodoListViewTest {
+    public static void main(String[] args) {
+        testAddTodoList();
+    }
+
+    public static void testShowTodoList(){
+        TodoListRepository todoListRepository = new TodoListRepositoryImpl();
+        TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
+        TodoListView todoListView = new TodoListView(todoListService);
+
+        todoListService.addTodoList("Belajar Golang");
+        todoListService.addTodoList("Belajar HTML");
+        todoListService.addTodoList("Belajar Apaaja");
+
+        todoListView.showTodoList();
+    }
+
+    public static void testAddTodoList(){
+        TodoListRepository todoListRepository = new TodoListRepositoryImpl();
+        TodoListService todoListService = new TodoListServiceImpl(todoListRepository);
+        TodoListView todoListView = new TodoListView(todoListService);
+
+        todoListView.addTodoList();
+        todoListService.showTodoList();
+        todoListView.addTodoList();
+        todoListService.showTodoList();
+    }
+}
